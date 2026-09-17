47e419cee9447f1729dfd72b7436a6d96c5d0548
PedroKnies <pedrokinis@gmail.com>
2026-09-16 21:21:11 -0300

    troquei pra versao anteriror o app.py

--- aula01/app.py
+++ aula01/app.py
@@ -6,6 +6,4 @@ import streamlit as st
 st.title("📚 Dashboard de Livros")
 st.write("Se você está vendo esta página, o seu ambiente está pronto! 🎉")
 
-
-st.write("testando 1.2.3 ")
-st.write("lero lero")
+#troquei pra versao anterior
