1afeb85c5fd1af02ce929deabe982044fa9bff45
PedroKnies <pedrokinis@gmail.com>
2026-09-16 21:27:03 -0300

    testando foto

--- aula01/app.py
+++ aula01/app.py
@@ -6,4 +6,8 @@ import streamlit as st
 st.title("📚 Dashboard de Livros")
 st.write("Se você está vendo esta página, o seu ambiente está pronto! 🎉")
 
+
+st.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXpRVTgXOfDxntIixJKFx4IfMavSxTtgy6tdU6LgsEB5JRl_PaKcVWnss&s=10")
+
+
 #troquei pra versao anterior
