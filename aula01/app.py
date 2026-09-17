7e701664b309b05b8e09b05a620650bfcbdbd2cc
PedroKnies <pedrokinis@gmail.com>
2026-09-16 21:17:45 -0300

    Atualiza app.py e requirements.txt

--- aula01/app.py
+++ aula01/app.py
@@ -5,3 +5,7 @@ import streamlit as st
 
 st.title("📚 Dashboard de Livros")
 st.write("Se você está vendo esta página, o seu ambiente está pronto! 🎉")
+
+
+st.write("testando 1.2.3 ")
+st.write("lero lero")
